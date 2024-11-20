2149f964f88f51960b27c2b844bbc986717739f3
ammellem <ammellem@mtu.edu>
2024-11-19 20:36:52 -0500

    Add files via upload

---
+++ Items.java
@@ -0,0 +1,73 @@
+/*
+* Item object class
+* Version 1.0 
+* @author Aaron Mellenma
+*/
+
+public class Items {
+
+private int dmg;
+private int durr;
+private int weight;
+private String name;
+private String special;
+
+/*
+* Constructors
+*/
+
+public Items(String name, int dmg, int durr, int weight, String special) {
+
+	this.name = name;
+	this.dmg = dmg;
+	this.durr = durr;
+	this.weight = weight;
+	this.special = special;
+
+}
+
+public Items(String name, int dmg, int durr, int weight) {
+
+	this.name = name;
+	this.dmg = dmg;
+	this.durr = durr;
+	this.weight = weight;
+
+}
+
+public Items (String name, String special) {
+
+	this.name = name;
+	this.special = special;
+
+}
+
+/*
+* Return Methods
+*/
+
+public int returnDmg() { return dmg; }
+
+public int returnDurr() { return durr; }
+
+public int returnWeight() { return weight; }
+
+public String returnName() { return name; }
+
+public String returnSpecial() { return special; }
+
+public String returnParam() {
+	
+	String r = "Name: " + name;
+	r += dmg > 0 ? "\nDamage: " + dmg : "";
+	r += durr > 0 ? "\nStructure Damage: " + durr : "";
+	r += weight > 0 ? "\nWeight: " + weight : "";
+	r += special == null ? "" : "\nSpecial ability: " + special;
+
+	return r;
+	
+}
+
+
+
+}
