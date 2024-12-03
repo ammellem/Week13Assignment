ea893039082d9c474f7bbe19c3a28d2ac1a5e37b
Austin Deegan <ardeegan@mtu.edu>
2024-12-03 18:53:16 -0500

    Add files via upload

--- GameRunner.java
+++ GameRunner.java
@@ -11,23 +11,29 @@ public class GameRunner {
 		if ( args.length == 1 ) 
 			args = new String[] { args[ 0 ], "2112" };
 		if ( args.length != 2 ) {
-			System.out.println( "Command line arguments: server_address port" );
+		   System.out.println( "Command line arguments: server_address port" );
 		} else {
-			try ( Socket server = new Socket ( args[0], Integer.valueOf ( args[1] ) ) ) {
-				System.out.println( "Connected to AdventureServer host " + server.getInetAddress () );
-				BufferedReader fromServer = new BufferedReader ( new InputStreamReader ( server.getInputStream () ) );
-				PrintWriter toServer = new PrintWriter ( server.getOutputStream (), true );
-				BufferedReader keyboardInput = new BufferedReader ( new InputStreamReader ( System.in ) );
+			try ( Socket server = new Socket ( args[0], 
+				Integer.valueOf ( args[1] ) ) ) {
+				System.out.println( "Connected to AdventureServer host " + 
+					server.getInetAddress () );
+				BufferedReader fromServer = new BufferedReader 
+					( new InputStreamReader ( server.getInputStream () ) );
+				PrintWriter toServer = new PrintWriter 
+					( server.getOutputStream (), true );
+				BufferedReader keyboardInput = new BufferedReader 
+					( new InputStreamReader ( System.in ) );
 				String s = " ";
 				System.out.print( "\033[H\033[2J" ); // clear terminal
-				while ( !s.equalsIgnoreCase( "quit" ) ) {
+				while ( !s.equalsIgnoreCase( "quit" ) && 
+					!s.equals( "SHUTDOWN" ) ) {
 					
 					while (!s.equals( "" ) ){
 						s = fromServer.readLine ();
 						System.out.println( s );
 						if ( s.endsWith( "quiting" ) ) break;
 					}
-					System.out.print("> ");
+					System.out.print( "> " );
 					System.out.flush ();
 					if ( s.endsWith( "quiting" ) ) break;
 					s=keyboardInput.readLine ();
@@ -36,6 +42,8 @@ public class GameRunner {
 				}
 				if ( s.equalsIgnoreCase( "quit" ) ) 
 					System.out.println( "quiting\n" );
+				if ( s.equals( "SHUTDOWN" ) ) 
+					System.out.println( "Server shutdown. Quiting\n" );
 				fromServer.close ();
 				toServer.close ();
 				keyboardInput.close ();
